e47640454eb1496deae1cd7e34224f833c4b4b03
Carlos Escudero Garcia <cescuder@alpha20.aulas.gsyc.urjc.es>
2019-04-09 19:25:23 +0200

    9abril

--- Practica-2/mi_proyectoweb/mi_tienda/urls.py
+++ Practica-2/mi_proyectoweb/mi_tienda/urls.py
@@ -12,10 +12,9 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('mi_tienda.urls') ),
+    url(r'^$', views.home_view),
 ]
